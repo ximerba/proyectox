7ed79f32d25d142f7e27629e556df79a5176b777
Ximena <ximerba@gmail.com>
2016-09-24 14:50:44 -0300

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -3,7 +3,7 @@
 define('DB_SERVER','localhost');
 define('DB_NAME','proyectox');
 
-$con = mysql_connect('localhost','root','') or die ('No se pudo conectar'. mysql_error());
+$conexion = mysql_connect('localhost','root','') or die ('No se pudo conectar'. mysql_error());
 echo 'Se conecto correctamente';
-mysql_select_db('proyectox',$con) or die ("Ocurrio un error, por favor intente de nuevo más tarde".mysql_error($conexion));
+mysql_select_db('proyectox',$conexion) or die ("Ocurrio un error, por favor intente de nuevo más tarde".mysql_error($conexion));
 ?>
